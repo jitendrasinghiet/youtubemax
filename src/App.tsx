18ba467808bacfccca88eaab2d6f658199a091e1
jitendrasinghiet <29883420+jitendrasinghiet@users.noreply.github.com>
2026-08-13 01:11:42 +0530

    relevance

--- src/App.tsx
+++ src/App.tsx
@@ -216,7 +216,7 @@ function App() {
     setSearchError(null)
     setSearchSoftWarning(null)
     setSearchResults([])
-    setSearchSortType('recommended')
+    setSearchSortType('relevance')
 
     try {
       const { results, warning } = await searchVideos(input)
